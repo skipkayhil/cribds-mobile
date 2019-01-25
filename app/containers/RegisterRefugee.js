35f351f63cc3237ca5c79f49c96d26c7dd95a988
SkipKayhil <skipkayhil@gmail.com>
2019-01-25 11:37:13 -0500

    update Register Refugee, remove npm lock

--- app/containers/RegisterRefugee.js
+++ app/containers/RegisterRefugee.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Button, Text } from 'native-base';
+import { Button, Content, Text, Form, Item, Label, Input } from 'native-base';
 import { BackButton, NavigationHeader } from '../components';
 
 const RegisterRefugee = props => {
   return (
-    <Button
-      full
-      success
-      onPress={() => props.navigation.navigate('EmployeeHome')}
-    >
-      <Text>Submit</Text>
-    </Button>
+    <Content>
+      <Form style={{ marginRight: 15 }}>
+        <Item floatingLabel>
+          <Label>Name</Label>
+          <Input />
+        </Item>
+        <Item floatingLabel>
+          <Label>Email</Label>
+          <Input keyboardType="email-address" />
+        </Item>
+      </Form>
+      <Button
+        full
+        success
+        style={{ marginTop: 30 }}
+        onPress={() => props.navigation.navigate('EmployeeHome')}
+      >
+        <Text>Submit</Text>
+      </Button>
+    </Content>
   );
 };
 
