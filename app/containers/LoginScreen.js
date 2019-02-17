d3491d38f2d2d1a50c1a6f23424832c23a5ac8d9
tjones320 <tjones320@gatech.edu>
2019-02-17 00:41:06 -0500

    First attempt to get the login screen up and running

--- app/containers/LoginScreen.js
+++ app/containers/LoginScreen.js
@@ -1,30 +1,82 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Button, Text } from 'native-base';
+import React, { Component } from 'react';
+import { Alert, Button, TextInput, View, StyleSheet, Picker } from 'react-native';
+
+export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      username: '',
+      password: '',
+      userType: 'Refugee'
+    };
+  }
+  
+  onLogin() {
+    const { username, password, userType} = this.state;
+    const {navigate} = this.props.navigation
+    //navigation(userType, {name : 'Home'});
+    if (true) {
+      Alert.alert('Credentials', `${username} + ${password} + ${userType}`);
+    }
+  }
+
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          maxLength={20}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          maxLength={20}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+          //onPress={() => navigate(this.state.userType, {name: 'Home'})}
+        />
+        <Picker
+          selectedValue={this.state.userType}
+          style={{height: 50, width: 200}}
+          onValueChange={(itemValue, itemIndex) =>
+          this.setState({userType: itemValue})
+          }>
+          <Picker.Item label="Refugee" value='Refugee' />
+          <Picker.Item label="Employee" value='Employee' />
+          <Picker.Item label="Admin" value='Admin' />
+        </Picker>
+
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-around'
-  }
+    justifyContent: 'center',
+    backgroundColor: '#9c75f4',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+
+  },
 });
-
-const LoginScreen = props => {
-  return (
-    <Container style={styles.container}>
-      <Text>Login Screen</Text>
-      <Button full onPress={() => props.navigation.navigate('Employee')}>
-        <Text>HSS Employee</Text>
-      </Button>
-      <Button full onPress={() => props.navigation.navigate('Refugee')}>
-        <Text>Refugee</Text>
-      </Button>
-      <Button full onPress={() => props.navigation.navigate('Admin')}>
-        <Text>Administrator</Text>
-      </Button>
-    </Container>
-  );
-};
-
-export default LoginScreen;
