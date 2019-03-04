18129c69f3665d8e2595977cc6a115833a6af021
skipkayhil <skipkayhil@gmail.com>
2019-03-03 19:39:28 -0500

    fix android header in status bar

--- app/components/NavigationHeader.js
+++ app/components/NavigationHeader.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Platform, StatusBar } from 'react-native';
 import { Header, Left, Body, Right, Title } from 'native-base';
 
+const androidStyle = {
+  paddingTop: StatusBar.currentHeight,
+  height: 54 + StatusBar.currentHeight
+};
+
 const NavigationHeader = ({ title, left, right }) => (
-  <Header>
+  <Header style={Platform.OS === 'android' ? androidStyle : {}}>
     <Left>{left}</Left>
     <Body style={{ flex: 3 }}>
       <Title>{title}</Title>
