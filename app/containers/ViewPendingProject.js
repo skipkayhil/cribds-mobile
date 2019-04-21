0856f6ec7dd6a661fd5e07e0535c37943551516d
aattss <naohu@live.com>
2019-04-21 14:40:16 -0400

    approving/rejecting projects sort of works now

--- app/containers/ViewPendingProject.js
+++ app/containers/ViewPendingProject.js
@@ -1,56 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { Button, Icon, Input, Form, Item, Label, Text } from 'native-base';
 import { BackButton, NavigationHeader } from '../components';
 
-onAccept = (props) => {
-  const proj = props.project;
-  proj.status = 'approved';
-  props.firestore.set({collection: 'projects', doc:props.navigation.state.params.uid}, proj)
 
-};
+class ViewPendingProject extends Component {
+  state = {
+    project: { ...this.props.project},
+    type: { ...this.props.type},
+    refugee: { ...this.props.refugee}
+  }
 
-const ViewPendingProject = props => {
-  return (
-  <Form style={{ marginRight: 15 }}>
-    <Item floatingLabel>
-      <Label>Title:</Label>
-      <Input disabled value={props.project.title} />
-    </Item>
-    <Item floatingLabel>
-      <Label>Creator:</Label>
-      <Input disabled value={props.refugee.first_name + " " + props.refugee.last_name} />
-    </Item>
-    <Item floatingLabel>
-      <Label>Project Type:</Label>
-      <Input disabled value={props.type.name} />
-    </Item>
-    <Item floatingLabel>
-      <Label>Submitted:</Label>
-      <Input disabled value={props.project.submission_date.toDate() + ""} />
-    </Item>
-    <Item floatingLabel>
-      <Label>Funding Requested:</Label>
-      <Input disabled value={props.project.funds_needed + ""} />
-    </Item>
-    <Item floatingLabel>
-      <Label>Funding Recieved:</Label>
-      <Input disabled value={props.project.funds_acquired + ""} />
-    </Item>
-    <Item floatingLabel>
-      <Label>Details:</Label>
-      <Input disabled/>
-    </Item>
-    <Text> {props.project.description} </Text>
-    <Button full success onPress={onAccept(props)}>
-      <Text>
-        {'Approve Project'}
-      </Text>
-    </Button>
-  </Form>
-)};
+  onAccept = (props) => {
+    this.props.firestore.update({collection: 'projects', doc:this.props.navigation.state.params.uid}, {status: 'approved'});
+
+  };
+
+  onReject = (props) => {
+    this.props.firestore.update({collection: 'projects', doc:this.props.navigation.state.params.uid}, {status: 'rejected'});
+
+  };
+
+  render() {
+    const { project, refugee, type} = this.state;
+    return (
+    <Form style={{ marginRight: 15 }}>
+      <Item floatingLabel>
+        <Label>Title:</Label>
+        <Input disabled value={project.title} />
+      </Item>
+      <Item floatingLabel>
+        <Label>Creator:</Label>
+        <Input disabled value={refugee.first_name + " " + refugee.last_name} />
+      </Item>
+      <Item floatingLabel>
+        <Label>Project Type:</Label>
+        <Input disabled value={type.name} />
+      </Item>
+      <Item floatingLabel>
+        <Label>Submitted:</Label>
+        <Input disabled value={project.submission_date.toDate() + ""} />
+      </Item>
+      <Item floatingLabel>
+        <Label>Funding Requested:</Label>
+        <Input disabled value={project.funds_needed + ""} />
+      </Item>
+      <Item floatingLabel>
+        <Label>Funding Recieved:</Label>
+        <Input disabled value={project.funds_acquired + ""} />
+      </Item>
+      <Item floatingLabel>
+        <Label>Details:</Label>
+        <Input disabled/>
+      </Item>
+      <Text> {project.description} </Text>
+      <Button full success onPress={this.onAccept}>
+        <Text>
+          {'Approve Project'}
+        </Text>
+      </Button>
+
+      <Button full success onPress={this.onReject}>
+        <Text>
+          {'Reject Project'}
+        </Text>
+      </Button>
+    </Form>
+)}};
 
 ViewPendingProject.navigationOptions = ({ navigation }) => ({
   header: <NavigationHeader title="Project Details" left={<BackButton />} />
